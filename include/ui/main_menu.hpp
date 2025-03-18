57830ed6d5ee226f088c068b1008eb7a83c3d18b
Moroz Roman <morozdef24@gmail.com>
2025-03-19 00:52:20 +0300

    main menu

---
+++ include/ui/main_menu.hpp
@@ -0,0 +1,21 @@
+#pragma once
+
+#include "../database/database.hpp"
+#include "colors.hpp"
+
+namespace menu {
+
+class MainMenu {
+private:
+    database::Database& db;
+    
+public:
+    explicit MainMenu(database::Database& database);
+
+    ~MainMenu() = default;
+
+    void run();
+
+}; // class MainMenu
+
+} // namespace menu
