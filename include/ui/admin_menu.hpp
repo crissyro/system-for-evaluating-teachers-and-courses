84c76167188c0afb124beb6d0d65e107fffc5314
Moroz Roman <morozdef24@gmail.com>
2025-03-19 00:17:45 +0300

    admin_menu.hpp

---
+++ include/ui/admin_menu.hpp
@@ -0,0 +1,24 @@
+#pragma once
+
+#include "../database/database.hpp"
+#include "colors.hpp"
+
+namespace menu {
+    
+class AdminMenu {
+    database::Database& db;
+    
+public:
+    explicit AdminMenu(database::Database& database);
+
+    ~AdminMenu() = default;
+
+    void show();
+
+    void showAllRatings();
+
+    void showStatistics();
+
+}; // AdminMenu
+
+} // namespace menu
