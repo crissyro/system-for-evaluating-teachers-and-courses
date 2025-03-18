15bd3cb7319d07aa8417d45e0c2c078890fa305d
Moroz Roman <morozdef24@gmail.com>
2025-03-18 22:57:18 +0300

    teacher.hpp

---
+++ include/classes/teacher.cpp
@@ -0,0 +1,37 @@
+#pragma once
+
+#include <string>
+
+namespace teacher {
+class Teacher {
+    int id;
+    std::string surname;
+    std::string name;
+    std::string patronymic;
+    std::string institute;
+    std::string department;
+
+public:
+    Teacher(int id, const std::string& surname, const std::string& name,
+           const std::string& patronymic, const std::string& institute, 
+           const std::string& department);
+
+    inline int getId() const { return id; }
+
+    inline std::string getSurname() const { return surname; }
+
+    inline std::string getName() const { return name; }
+
+    inline std::string getPatronymic() const { return patronymic; }
+
+    inline std::string getInstitute() const { return institute; }
+
+    inline std::string getDepartment() const { return department; }
+
+    inline std::string getFullName() const {
+        return surname + " " + name + " " + patronymic;
+    }
+
+}; // Teacher
+
+} // namespace teacher
