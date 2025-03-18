85a31e82a9da4c6fba0b84d9dd669ce8c6b5c292
Moroz Roman <morozdef24@gmail.com>
2025-03-19 00:15:46 +0300

    admin_db.cpp realization

---
+++ src/database/admin_db.cpp
@@ -0,0 +1,44 @@
+#include "database/admin_db.hpp"
+#include <stdexcept>
+
+namespace database {
+
+AdminDB::AdminDB(const Database& database) : db(database.getHandle()) {
+    const char* sql = R"(
+        CREATE TABLE IF NOT EXISTS admins (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            username TEXT UNIQUE NOT NULL,
+            password TEXT NOT NULL
+        );)";
+
+    Database::Statement stmt(db, sql);
+    sqlite3_step(stmt);
+}
+
+bool AdminDB::addAdmin(const std::string& username, const std::string& password) {
+    const std::string ADMIN_PASSWORD = "admin123";
+    
+    const char* sql = "INSERT INTO admins (username, password) VALUES (?, ?);";
+
+    Database::Statement stmt(db, sql);
+    stmt.bind(1, username);
+    stmt.bind(2, ADMIN_PASSWORD);
+    
+    return sqlite3_step(stmt) == SQLITE_DONE;
+}
+
+bool AdminDB::checkCredentials(const std::string& username, const std::string& password) {
+    const char* sql = "SELECT password FROM admins WHERE username = ?;";
+
+    Database::Statement stmt(db, sql);
+    stmt.bind(1, username);
+    
+    if(sqlite3_step(stmt) == SQLITE_ROW) {
+        std::string stored_password = reinterpret_cast<const char*>(sqlite3_column_text(stmt, 0));
+        return password == "admin123"; 
+    }
+    
+    return false;
+}
+
+} // namespace database
