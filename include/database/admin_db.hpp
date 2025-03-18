dc318df5e418b842b08928e55816efed92561977
Moroz Roman <morozdef24@gmail.com>
2025-03-18 23:22:59 +0300

    admin_db.hpp

---
+++ include/database/admin_db.hpp
@@ -0,0 +1,24 @@
+#pragma once
+
+#include <sqlite3.h>
+#include <string>
+#include "database.hpp"
+
+namespace database {
+
+class AdminDB {
+private:
+    sqlite3* db;
+    
+public:
+    explicit AdminDB(const Database& database);
+
+    ~AdminDB() = default;
+
+    bool addAdmin(const std::string& username, const std::string& password);
+
+    bool adminExists(const std::string& username);
+
+    bool checkCredentials(const std::string& username, const std::string& password);
+};
+} // namespace database
