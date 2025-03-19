44cbc481be77331e6414ab690e153e5982a23a0f
Moroz Roman <morozdef24@gmail.com>
2025-03-19 23:03:16 +0300

    login_menu create

---
+++ src/ui/login_menu.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+
+#include "../include/ui/login_menu.hpp"
+#include "../include/database/student_db.hpp"
+#include "../include/database/admin_db.hpp"
+
+namespace menu {
+
+LoginMenu::LoginMenu(database::Database& database) : db(database) {}
+
+int LoginMenu::loginStudent() {
+    std::string recordBook;
+    std::cout << CYAN << "Введите номер зачетки: " << RESET;
+    std::getline(std::cin, recordBook);
+    
+    database::StudentDB studentDB(db);
+    if (studentDB.exists(recordBook)) {
+        return studentDB.getStudentIdByRecordBook(recordBook);
+    }
+    std::cout << RED << "Студент не найден!\n" << RESET;
+    return -1;
+}
+
+bool LoginMenu::loginAdmin() {
+    std::string username;
+    std::cout << CYAN << "Логин: " << RESET;
+
+    std::getline(std::cin, username);
+
+    std::string password;
+    std::cout << CYAN << "Пароль: " << RESET;
+
+    std::getline(std::cin, password);
+    
+    database::AdminDB adminDB(db);
+
+    return adminDB.checkCredentials(username, password);
+}
+
+} 
