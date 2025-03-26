c8053e6b12c7c2898e8d5e9f0b10bce8c04c4070
Moroz Roman <morozdef24@gmail.com>
2025-03-27 00:11:15 +0300

    add build script

--- scripts/backup_db.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-DATE=$(date +%Y%m%d)
-BACKUP_DIR="backups"
-
-mkdir -p $BACKUP_DIR
-cp university.db $BACKUP_DIR/university_$DATE.db
-echo "Backup created: $BACKUP_DIR/university_$DATE.db"
